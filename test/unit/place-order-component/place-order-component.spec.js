0ed4fc4bec76549006a6d36c476b1119f0658b7b
Nico Jansen <jansennico@gmail.com>
2018-04-11 15:41:56 +0200

    test: place order test

--- test/unit/place-order-component/place-order-component.spec.js
+++ test/unit/place-order-component/place-order-component.spec.js
@@ -1,14 +1,8 @@
-describe('PlaceOrderComponent', function() {
+describe('PlaceOrderComponent', function () {
 
-    beforeEach(module('robobar'));
-  
-    describe('PlaceOrderComponent', function() {
-  
-      it('should create a `phones` model with 3 phones', inject(function($componentController) {
-        var ctrl = $componentController('roboPlaceOrder');
-        expect(ctrl.drinks.length).toBe(3);
-      }));
-  
-    });
-  
-  });
+  beforeEach(module('robobar'));
+  it('should have 3 drinks', inject(function ($componentController) {
+    var ctrl = $componentController('roboPlaceOrder');
+    expect(ctrl.drinks.length).toBe(3);
+  }));
+});
