ce9003f73f297709eb176ecd959fae8544ced90d
Nico Jansen <jansennico@gmail.com>
2022-09-16 22:07:19 +0200

    Fix drink-service url

--- src/services/drink.service.js
+++ src/services/drink.service.js
@@ -4,7 +4,7 @@ export class DrinkService {
    * @returns {Promise<Drink[]>}
    */
   async getDrinks() {
-    const response = await fetch('/api/drinks.json');
+    const response = await fetch('api/drinks.json');
     return response.json();
   }
 }
